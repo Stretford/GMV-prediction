2c2076570847932cdc57451ae7e99a868ce4b134
Stretford <stretford@aas-MacBook-Pro.local>
2014-07-10 15:36:15 +0800

    agamogenesis

--- chromosome.py
+++ chromosome.py
@@ -5,52 +5,53 @@ import random
 
 
 class Gaussian:  # for a single gaussian function:(center, width, degree)
-    Center = 0
-    Width = 0
-    Degree = 0  # degree of usefulness
+    center = 0
+    width = 0
+    degree = 0  # degree of usefulness
 
     def __init__(self, center, width):
-        self.Center = center
-        self.Width = width
+        self.center = center
+        self.width = width
         #self.Degree = 0
 
     def __str__(self):
-        return ''.join(['(', str(self.Center), ',', str(self.Width), ')'])
+        return ''.join(['(', str(self.center), ',', str(self.width), ')'])
 
     def cal_degree(self, x):
-        self.Degree = tool.gaussian(self.Center, self.Width, x)
-        return self.Degree
+        self.degree = tool.gaussian(self.center, self.width, x)
+        return self.degree
 
 
 class Rule:  # for a single rule: IF A IS GAUSSIAN(...) AND B IS GAUSSIAN(...) THEN Y IS GAUSSIAN(...)
-    Gaussian1 = Gaussian(0, 0)
-    Gaussian2 = Gaussian(0, 0)
-    GaussianY = Gaussian(0, 0)
+    gaussian1 = Gaussian(0, 0)
+    gaussian2 = Gaussian(0, 0)
+    gaussian_y = Gaussian(0, 0)
 
-    def __init__(self, gaussian1, gaussian2, gaussiany):
-        self.Gaussian1 = gaussian1
-        self.Gaussian2 = gaussian2
-        self.GaussianY = gaussiany
+    def __init__(self, gaussian1, gaussian2, gaussian_y):
+        self.gaussian1 = gaussian1
+        self.gaussian2 = gaussian2
+        self.gaussian_y = gaussian_y
 
     def __repr__(self):
-        return ''.join(['IF A IS ', str(self.Gaussian1), ' AND B IS ', str(self.Gaussian2), ' THEN Y IS ', str(self.GaussianY)])
+        return ''.join(['IF A IS ', str(self.gaussian1),
+                        ' AND B IS ', str(self.gaussian2), ' THEN Y IS ', str(self.gaussian_y)])
 
-    def Defuzzify(self, x1, x2):
-        degree1 = tool.gaussian(self.Gaussian1.Center, self.Gaussian1.Width, x1)
-        degree2 = tool.gaussian(self.Gaussian2.Center, self.Gaussian2.Width, x2)
+    def defuzzify(self, x1, x2):
+        degree1 = tool.gaussian(self.gaussian1.center, self.gaussian1.width, x1)
+        degree2 = tool.gaussian(self.gaussian2.center, self.gaussian2.width, x2)
         return degree1 * degree2
 
 
 class Chromosome:  # for a single chromosome consisting of several rules
-    ruleNum = 0
+    rule_num = 0
     rule_sets = []
 
-    def __init__(self, rulenum, rulesets=[]):
-        if rulesets:
-            self.rule_sets = rulesets
-            self.ruleNum = rulenum
+    def __init__(self, rule_num, rule_sets=[]):
+        if rule_sets:
+            self.rule_sets = rule_sets
+            self.rule_num = rule_num
         else:
-            for i in range(0, rulenum):
+            for i in range(0, rule_num):
                 self.rule_sets.append(-1)
 
     def __repr__(self):
@@ -67,31 +68,40 @@ class Chromosome:  # for a single chromosome consisting of several rules
 
     def mutate(self):
         rule = self.rule_sets[random.randint(0, len(self.rule_sets) - 1)]
-        g_sets = [rule.Gaussian1, rule.Gaussian2, rule.GaussianY]
+        g_sets = [rule.gaussian1, rule.gaussian2, rule.gaussian_y]
         gaussian = random.choice(g_sets)
         if random.random() > 0.5:
-            gaussian.Center *= 2 * random.random()
+            gaussian.center *= 2 * random.random()
         else:
-            gaussian.Width *= 2 * random.random()
+            gaussian.width *= 2 * random.random()
+
+    def fitness(self, inputs):  # calculate the fitness value of a chromosome for GA selection
+        mse = 0
+        for x1, x2, y in inputs:
+            y_predict = self.defuzzify(x1, x2)
+            mse += (y_predict - y) ** 2
+        mse /= len(inputs)
+        return mse
 
 
-def randomize_rule(center_min, center_max, N_init):
-    max_width = 2 * (center_max - center_min) / (N_init ** 0.5)
+def randomize_rule(center_min, center_max, n_init): # initialize a randomized rule given the bounding range
+    max_width = 2 * (center_max - center_min) / (n_init ** 0.5)
     g1 = Gaussian(random.uniform(center_min, center_max), random.uniform(0, max_width))
     g2 = Gaussian(random.uniform(center_min, center_max), random.uniform(0, max_width))
     gy = Gaussian(random.uniform(center_min, center_max), random.uniform(0, max_width))
     return Rule(g1, g2, gy)
 
 
-def randomize_chromosome(min, max, rulenum, n_init):
+def randomize_chromosome(minimum, maximum, rule_num, n_init):
+# initialize a randomized chromosome given the bounding range
     rule_sets = []
-    for i in range(0, rulenum):
-        rule = randomize_rule(min, max, n_init)
+    for i in range(0, rule_num):
+        rule = randomize_rule(minimum, maximum, n_init)
         rule_sets.append(rule)
-    return Chromosome(rulenum, rule_sets)
+    return Chromosome(rule_num, rule_sets)
 
 
-def intercross(p1, p2):
+def intercross(p1, p2):  # intercross of two chromosomes for GA
     intercross_point = 2
     c = [i for i in range(0, intercross_point)]
     for i in range(0, intercross_point):
@@ -100,10 +110,7 @@ def intercross(p1, p2):
         p2.rule_sets[i] = c[i]
 
 
-
-
-
-c = randomize_chromosome(900,1200,3,10)
+c = randomize_chromosome(900, 1200, 3, 10)
 print(c)
 print(' ')
 c.mutate()
--- population.py
+++ population.py
@@ -1,5 +1,6 @@
 __author__ = 'stretford'
 import chromosome
+import random
 
 
 class population:
@@ -9,17 +10,48 @@ class population:
     agamogenesis_rate = 0
     mutate_rate = 0
     rule_num = 0
-    best_gene = chromosome.Chromosome()
+    best_gene = chromosome.Chromosome(7)
 
-    def __init__(self, intercrossRate, agamogenesisRate, mutateRate, rulenum):
-        self.intercross_rate = intercrossRate
-        self.agamogenesis_rate = agamogenesisRate
-        self.mutate_rate = mutateRate
+    def __init__(self, intercross_rate, agamogenesis_rate, mutate_rate, rulenum):
+        self.intercross_rate = intercross_rate
+        self.agamogenesis_rate = agamogenesis_rate
+        self.mutate_rate = mutate_rate
         self.rule_num = rulenum
         #initializing rule sets
         for i in range(0, self.colony_size):
             c = chromosome.randomize_chromosome(4000, 7000, 7, 18)
             self.colony.append(c)
+
+    def intercross(self):
+        intercross_times = int(self.intercross_rate * self.colony_size)
+        for i in range(0, intercross_times):
+            rand1 = random.randint(0, self.colony_size)
+            rand2 = random.randint(0, self.colony_size)
+            if rand1 == rand2:
+                rand2 = (rand2 + 1) % self.colony_size
+            chromosome.intercross(self.colony[rand1], self.colony[rand2])
+
+    def agamogenesis(self):
+        agamogenesis_times = int(self.agamogenesis_rate * self.colony_size)
+        for i in range(0, agamogenesis_times):
+            self.colony.append(self.best_gene)
+
+    def mutate(self):
+        mutate_times = int(self.mutate_rate * self.colony_size)
+        for i in range(0, mutate_times):
+            unfortunate = random.choice(self.colony)
+            unfortunate.mutate()
+
+    def keep_size(self):
+        sorted_dict = {}
+        #for c in self.colony:
+            #sorted_dict[c.]
+
+    def next_generation(self):
+        self.intercross()
+        self.agamogenesis()
+        self.mutate()
+
 
     def __str__(self):
         return "\n".join(repr(c) for c in self.colony)
